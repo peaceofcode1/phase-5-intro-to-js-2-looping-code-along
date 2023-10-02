6db61ce02991c6550e4893d7bac0e8585560c142
jafar bino <binocoder@gmail.com>
2023-10-02 05:04:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+      thankYouMessages.push(message);
+    }
+    return thankYouMessages;
+  }
+  
+ 
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+ 
+  const names = ["Alice", "Bob", "Charlie"];
+  const event = "birthday";
+  const messages = writeCards(names, event);
+  console.log(messages);
+  
+
+  const startingNumber = 5;
+  countDown(startingNumber);
+  
